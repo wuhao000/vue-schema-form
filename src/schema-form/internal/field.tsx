087ffbf02bab2be9aea39a64029b7d173a64f9cd
wuhao <wh1200@126.com>
2020-07-25 01:52:57 +0800

    '修复textarea输入中文不触发input事件导致输入内容丢失的问题'

--- src/schema-form/internal/field.tsx
+++ src/schema-form/internal/field.tsx
@@ -113,8 +113,8 @@ export default class FormField extends mixins(Emitter) {
         this.field.visible = definition.depends(val);
       } else {
         return !definition.depends
-          .map(condition => matchCondition(val, condition))
-          .some(it => !it);
+            .map(condition => matchCondition(val, condition))
+            .some(it => !it);
       }
     }
   }
@@ -233,39 +233,39 @@ export default class FormField extends mixins(Emitter) {
       delete arrayProps.style;
       // @ts-ignore
       return <ArrayComponent
-        props={arrayProps}
-        class={arrayClass}
-        style={arrayStyle}
-        disabled={isDisabled}
-        subForm={field.type === TYPES.object}
-        addBtnText={props.addBtnText}
-        ref="array"
-        key={field.plainPath}
-        platform={platform}
-        addBtnProps={props.addBtnProps}
-        cellSpan={props.cellSpan}
-        onRemove={async (index) => {
-          try {
-            const confirmFunc = getConfirmFunction(platform);
-            await confirmFunc('确定删除该条吗？', '提示');
-            this.removeArrayItem(index);
-          } catch (e) {
-            console.error(e);
-          }
-        }}
-        onMoveDown={(index) => {
-          if (index <= currentValue.length - 1) {
-            swap(currentValue, index, index + 1);
-          }
-        }}
-        onMoveUp={(index) => {
-          if (index > 0) {
-            swap(currentValue, index, index - 1);
-          }
-        }}
-        onAdd={() => {
-          this.addArrayItem();
-        }}>
+          props={arrayProps}
+          class={arrayClass}
+          style={arrayStyle}
+          disabled={isDisabled}
+          subForm={field.type === TYPES.object}
+          addBtnText={props.addBtnText}
+          ref="array"
+          key={field.plainPath}
+          platform={platform}
+          addBtnProps={props.addBtnProps}
+          cellSpan={props.cellSpan}
+          onRemove={async (index) => {
+            try {
+              const confirmFunc = getConfirmFunction(platform);
+              await confirmFunc('确定删除该条吗？', '提示');
+              this.removeArrayItem(index);
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+          onMoveDown={(index) => {
+            if (index <= currentValue.length - 1) {
+              swap(currentValue, index, index + 1);
+            }
+          }}
+          onMoveUp={(index) => {
+            if (index > 0) {
+              swap(currentValue, index, index - 1);
+            }
+          }}
+          onAdd={() => {
+            this.addArrayItem();
+          }}>
         {
           currentValue ? currentValue.map((v, index) => {
             const itemProps = Object.assign({}, props, {
@@ -282,19 +282,19 @@ export default class FormField extends mixins(Emitter) {
             delete itemProps.style;
             // @ts-ignore
             return <InputFieldComponent
-              attrs={itemProps}
-              class={className}
-              style={style}
-              arrayIndex={index}
-              disabled={isDisabled}
-              key={field.plainPath + '-' + index}
-              value={v}
-              title={platform === 'mobile' ? field.title : null}
-              onBlur={this.onBlur}
-              onFocus={this.onFocus}
-              onInput={(val) => {
-                onArrayItemInput(val, index);
-              }}/>;
+                attrs={itemProps}
+                class={className}
+                style={style}
+                arrayIndex={index}
+                disabled={isDisabled}
+                key={field.plainPath + '-' + index}
+                value={v}
+                title={platform === 'mobile' ? field.title : null}
+                onBlur={this.onBlur}
+                onFocus={this.onFocus}
+                onInput={(val) => {
+                  onArrayItemInput(val, index);
+                }}/>;
           }) : null
         }
       </ArrayComponent>;
@@ -303,7 +303,7 @@ export default class FormField extends mixins(Emitter) {
     props.value = currentValue;
     props.title = props.title || (platform === 'mobile' ? field.title : null);
     if (definition.type === TYPES.object
-      && definition.props) {
+        && definition.props) {
       if (!definition.props.props) {
         definition.props.props = {};
       }
@@ -334,21 +334,24 @@ export default class FormField extends mixins(Emitter) {
     }
     // @ts-ignore
     return <InputFieldComponent
-      props={props}
-      value={currentValue}
-      class={className}
-      attrs={props}
-      style={style}
-      nativeOn={nativeEvents}
-      on={Object.assign({
-        blur: this.onBlur,
-        focus: this.onFocus,
-        keydown: this.onKeydown,
-        keyup: this.onKeyup,
-        input: onInput
-      }, events)}
-      key={field.plainPath}
-      ref="input"/>;
+        props={props}
+        value={currentValue}
+        class={className}
+        attrs={props}
+        style={style}
+        nativeOn={nativeEvents}
+        on={Object.assign({
+          blur: this.onBlur,
+          focus: this.onFocus,
+          keydown: this.onKeydown,
+          keyup: this.onKeyup,
+          input: onInput,
+          compositionend: (e) => {
+            this.onInput(e.target.value);
+          }
+        }, events)}
+        key={field.plainPath}
+        ref="input"/>;
   }
 
   public onBlur(event) {
@@ -400,16 +403,16 @@ export default class FormField extends mixins(Emitter) {
       delete formItemProps.className;
       const noWrap = !field.title;
       const formItem = noWrap ? inputComponent :
-        <FormItemComponent attrs={Object.assign({}, formItemProps, {label: null})}
-                           class={className}
-                           style={style}>
-          {definition?.wrapperProps?.noTitle ? null :
-            <span slot="label">{formItemProps.label}</span>}
-          {inputComponent}
-          {
-            definition.description ? <div>{definition.description}</div> : null
-          }
-        </FormItemComponent>;
+          <FormItemComponent attrs={Object.assign({}, formItemProps, {label: null})}
+                             class={className}
+                             style={style}>
+            {definition?.wrapperProps?.noTitle ? null :
+                <span slot="label">{formItemProps.label}</span>}
+            {inputComponent}
+            {
+              definition.description ? <div>{definition.description}</div> : null
+            }
+          </FormItemComponent>;
       if (definition.span) {
         item = <ColComponent span={definition.span}>{formItem}</ColComponent>;
       } else {
@@ -481,8 +484,8 @@ export default class FormField extends mixins(Emitter) {
       if (definition.tip) {
         const popover = LibComponents.popover;
         props.label = <LibComponents.popover
-          content={definition.tip}
-          trigger="hover">
+            content={definition.tip}
+            trigger="hover">
           <span slot={popover === 'el-popover' ? 'reference' : 'default'}>
             {field.title}
             <LibComponents.icon style={{marginLeft: '5px', color: '#247dc5'}}
@@ -518,7 +521,7 @@ export default class FormField extends mixins(Emitter) {
     }
     const {field} = this;
     if (this.type === TYPES.object
-      && this.$refs.array) {
+        && this.$refs.array) {
       const array = this.$refs.array as any;
       const validateFields = array.$children.filter(it => (it as any).validate);
       return new Promise((resolve) => {
--- src/views/demo/feature/complex.vue
+++ src/views/demo/feature/complex.vue
@@ -1,215 +1,83 @@
 <template>
-  <div>
-<!--    <v-schema-form v-model="value"-->
-<!--                   :schema="schema"/>-->
-    <v-schema-form v-model="value2"
-                   :editable="false"
-                   :schema="schema2"/>
+  <div style="width: 600px"
+       class="bg-white">
+    {{value}}
+    <v-schema-form :schema="schema"
+                   v-model="value"/>
+    <a-mentions rows="3"
+                placeholder="You can use @ to ref user here"
+    >
+      <a-mentions-option value="afc163">
+        afc163
+      </a-mentions-option>
+      <a-mentions-option value="zombieJ">
+        zombieJ
+      </a-mentions-option>
+      <a-mentions-option value="yesmeck">
+        yesmeck
+      </a-mentions-option>
+    </a-mentions>
   </div>
 </template>
 <script lang="ts">
   import SchemaForm from '@/index';
-  import {SchemaFormField} from 'v-schema-form-types';
   import Vue from 'vue';
-
-  const schema = {
-    'props': {
-      'section': true,
-      'spaceBetweenSection': 16,
-      labelWidth: '120px'
-    },
-    'fields': [
-      {
-        'id': 'd43ae355-55d7-43ff-bdb4-edf85aebef22',
-        'property': 'd43ae355-55d7-43ff-bdb4-edf85aebef22.value',
-        'title': '选择项目',
-        'array': true,
-        'type': 'basic-data',
-        'placeholder': '请选择',
-        'props': {
-          'selectMode': 2,
-          'type': 'project'
-        },
-        'required': true,
-        'default': []
-      },
-
-      {
-        'id': '1ff99b08-b798-418f-9c4c-eecdc74493b6',
-        'property': '1ff99b08-b798-418f-9c4c-eecdc74493b6.value',
-        'title': '版本',
-        'array': false,
-        'type': 'string',
-        'placeholder': '请输入',
-        'props': {},
-        'required': true,
-        'default': ''
-      },
-
-      {
-        'id': '304e869b-7fc3-4ec9-975d-6f66a37700b5',
-        'property': '304e869b-7fc3-4ec9-975d-6f66a37700b5.value',
-        'title': '参与测试的团队',
-        'array': true,
-        'type': 'expand-select',
-        'placeholder': '',
-        'props': {},
-        'required': true,
-        'default': [],
-        'enum': [
-          {
-            'value': '1',
-            'label': '南京测试团队'
-          },
-          {
-            'value': '2',
-            'label': '北京测试团队'
-          },
-          {
-            'value': '3',
-            'label': '广州测试团队'
-          }
-        ]
-      },
-
-      {
-        'id': '4b2601f5-b606-489b-be5e-b49f9402b577',
-        'property': '4b2601f5-b606-489b-be5e-b49f9402b577.value',
-        'title': '更新类型',
-        'array': true,
-        'type': 'expand-select',
-        'placeholder': '',
-        'props': {},
-        'required': false,
-        'default': [],
-        'enum': [
-          {
-            'value': '1',
-            'label': '新增功能'
-          },
-          {
-            'value': '2',
-            'label': '缺陷修复'
-          },
-          {
-            'value': '3',
-            'label': '性能优化'
-          }
-        ]
-      },
-
-      {
-        'id': '0adf2aae-fe30-47bf-a8d8-2066953af768',
-        'property': '0adf2aae-fe30-47bf-a8d8-2066953af768.value',
-        'title': '紧急程度',
-        'array': false,
-        'type': 'select',
-        'placeholder': '',
-        'props': {},
-        'required': true,
-        'default': '',
-        'enum': [
-          {
-            'value': 'bca0054d-daa3-4fc1-9876-f4bde85250f6',
-            'label': '需要立即发布'
-          },
-          {
-            'value': '63f9c8f5-7518-4b2b-84c4-4d174c0246c8',
-            'label': '需要当日更新'
-          },
-          {
-            'value': '61479636-afa1-4e46-9b71-eb00b08a9b18',
-            'label': '随版本上线发布'
-          },
-          {
-            'value': '3764758b-b233-4be1-b22a-a246d36c7885',
-            'label': '非必须的发布'
-          }
-        ]
-      },
-
-      {
-        'id': 'a7ee2ac0-7d58-42d7-b381-23aa0df6ba82',
-        'property': 'a7ee2ac0-7d58-42d7-b381-23aa0df6ba82.value',
-        'title': '更新内容',
-        'array': false,
-        'type': 'text',
-        'placeholder': '请输入更新内容',
-        'props': {
-          'rows': 5
-        },
-        'required': true,
-        'default': ''
-      }
-    ]
-  };
-
-  const value = {
-    'd43ae355-55d7-43ff-bdb4-edf85aebef22': {
-      'value': [
-        'QD201903001'
-      ]
-    },
-    '1ff99b08-b798-418f-9c4c-eecdc74493b6': {
-      'value': '1.1'
-    },
-    '304e869b-7fc3-4ec9-975d-6f66a37700b5': {
-      'value': [
-        '1'
-      ]
-    },
-    '4b2601f5-b606-489b-be5e-b49f9402b577': {
-      'value': [
-        '1'
-      ]
-    },
-    '0adf2aae-fe30-47bf-a8d8-2066953af768': {
-      'value': 'bca0054d-daa3-4fc1-9876-f4bde85250f6'
-    },
-    'a7ee2ac0-7d58-42d7-b381-23aa0df6ba82': {
-      'value': '11111'
-    }
-  };
 
   export default Vue.extend({
     data() {
       return {
         value: null,
-        value2: value,
-        schema2: schema,
+        users: [],
+        showUserList: false,
         schema: {
           fields: {
-            a: {
+            obj: {
               type: 'object',
-              fields: [{
-                property: 'b.name',
-                type: 'string',
-                title: '标题'
-              }, {
-                property: 'b.value',
-                type: 'string',
-                title: '值'
-              }, {
-                property: 'c.d.e',
-                type: 'select',
-                props: {
-                  options: [{
-                    label: 'a', value: 1
-                  }, {
-                    label: 'b', value: 2
-                  }]
+              fields: {
+                'name.value': {
+                  type: 'text',
+                  title: '名称'
                 }
-              }]
+              }
             }
           }
-        } as SchemaFormField
+        }
       };
+    },
+    computed: {
+      align() {
+        return {
+          offset: [-200, 0]
+        };
+      }
+    },
+    methods: {
+      onInput(e) {
+        console.log(e);
+        if (e.data === '@') {
+          this.showUserList = true;
+        }
+      }
     },
     created() {
       SchemaForm.registerAntd();
     }
   });
 </script>
+<style lang="less">
+  .flow-steps {
+    .flow-step {
+      .am-steps-item-tail, .am-steps-item-icon {
+        margin-top: 15px;
+      }
 
+      .flow-step-title {
+      }
 
-
+      .flow-step-subtitle {
+        color: gray;
+        font-size: 80%;
+      }
+    }
+  }
+</style>
