c9a1616726938cf1ff3012519d24fceea2b2a654
吴昊 <wuhao@aegis-data.cn>
2020-08-13 00:32:48 +0800

    'fix'

--- test/test2.js
+++ test/test2.js
@@ -1,40 +1,16 @@
-const paths = [
-  ['a', 'b', 'c'],
-  ['a', 'b', 'e', 'f'],
-  ['a', 'b', 0, 'c'],
-  ['a', 'b', 0, 'd'],
-  ['a', 'b', 1, 'c'],
-  ['a', 'b', 2, 'c']
-];
-const def = {
-  fields: {
-    a: {
-      fields: {
-        b: {
-          props: {}
-        }
-      }
-    }
-  }
-};
-
-function searchDef(path) {
-  const parts = path.split('.');
-  let df = def;
-  parts.forEach(part => {
-    if (df.fields) {
-      if (typeof df.fields === 'object') {
-        df = df.fields[part];
-      } else if (Array.isArray(df.fields)) {
-        df = df.fields.find(it => it.property === part);
-      }
-    } else {
-      df = null;
-    }
+function req() {
+  return new Promise((resolve, reject) => {
+    console.log(resolve.toString());
+    console.log(reject.toString());
+    // resolve(1);
+    console.log(2)
+    reject(3);
   });
-  return df;
 }
 
-console.log(searchDef('a'));
-console.log(searchDef('a.b'));
-console.log(searchDef('a.b.c'));
+req();
+req().then((a) => {
+  console.log(a);
+}).catch(e => {
+  console.log(e);
+});
