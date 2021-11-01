7d967b7e8cd92612c53b343bbb33ce5fbbcd3623
吴昊 <wuhao@aegis-data.cn>
2021-11-02 00:34:47 +0800

    发布版本

--- src/generated/doc/biangengjilu/index.vue
+++ src/generated/doc/biangengjilu/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="markdown-body">
     <h2 id="更新日志">更新日志</h2>
+<h3 id="v30172">v3.0.172</h3>
+<p><code>2021.11.1</code>
+🌟 表单项选项去除forDisplay,forArray,forInput,layout等属性，使用mode和arrayMode代替
+🌟 副作用函数增加setStates用于代替enable,disable,editable,show,hide,readonly,required等方法，简化api</p>
+<h3 id="v30172-1">v3.0.172</h3>
+<p><code>2021.10.29</code>
+🌟 表单项支持显示序号
+🌟 支持使用vue组件作为type
+🌟 支持异步获取选项（enum）</p>
 <h3 id="v013">v0.1.3</h3>
 <p><code>2019.9.9</code></p>
-<ul>
-<li>表单项定义增加默认值（default属性）</li>
-<li>表单项为详情模式（editable为false）时，使用value操作赋值不会更新到表单的值对象</li>
-</ul>
+<p>🌟 表单项定义增加默认值（default属性）
+🌟 表单项为详情模式（editable为false）时，使用value操作赋值不会更新到表单的值对象</p>
 </div>
 </template>
